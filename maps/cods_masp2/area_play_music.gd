a027ecaacd9a5a55d7008d47f51e98701c2add24
brendonlucas <brendonplay007@gmail.com>
2020-03-22 17:01:36 -0300

    att

--- maps/cods_masp2/area_play_music.gd
+++ maps/cods_masp2/area_play_music.gd
@@ -6,14 +6,14 @@ var cam_animate
 
 func _ready():
 	music = get_parent().get_parent().get_parent().get_node("player/Player/target/music")
-	cam_animate= get_parent().get_parent().get_parent().get_node("player/Player/target/AnimationPlayer")
+	#cam_animate= get_parent().get_parent().get_parent().get_node("player/Player/target/AnimationPlayer")
 
 	
 func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "Player" and tocou != true:
-			cam_animate.play("intro")
+			#cam_animate.play("intro")
 			music.play()
 			tocou = true
 
